2e08df5c5aed8b606c4508b0072ad0e0e1c4272c
giovana <gi.siqueira0202@gmail.com>
2021-10-05 19:10:54 -0300

    upando projeto

---
+++ assets/javascript/js.js
@@ -0,0 +1,9 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '50px',
+    duration: 1000,
+    reset: true
+});
+
+ScrollReveal().reveal('.container', { delay: 300 });
+ScrollReveal().reveal('.box-info', { delay: 200 });
